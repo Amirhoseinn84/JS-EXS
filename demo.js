2e9bb7753ffe233c8bb1a72fc6457f671e770d44
Amirhosein Halimi <amirhoseinhalimy138444@gmail.com>
2023-04-15 12:27:37 +0430

    added project

--- demo.js
+++ demo.js
@@ -1,2 +1,16 @@
 let inputElement = document.getElementById('product-name');
 let charsElement = document.getElementById('remaining-chars');
+
+// console.dir(inputElement);
+
+let maxChars = inputElement.maxLength;
+
+function updateRemaining(event) {
+  let enteredText = event.target.value;
+  let enteredTextLength = enteredText.length;
+
+  let characters = maxChars - enteredTextLength;
+  charsElement.textContent = characters;
+}
+
+inputElement.addEventListener('input', updateRemaining);
