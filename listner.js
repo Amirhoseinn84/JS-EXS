ab0dba0408c5d686373a4ba0182a81e632491964
Amirhosein Halimi <amirhoseinhalimy138444@gmail.com>
2023-04-15 12:13:08 +0430

    added new project 2(etc)

--- listner.js
+++ listner.js
@@ -10,9 +10,9 @@ paragraphElement.addEventListener('click', changeElement);
 let inputElement = document.querySelector('input');
 
 function retrieveUserInput(event) {
-  // let enteredText = inputElement.value;
+  let enteredText = inputElement.value;
   // let enteredText = event.target.value;
-  let enteredText = event.data;
+  // let enteredText = event.data;
   console.log(enteredText);
   // console.log(event);
 }
