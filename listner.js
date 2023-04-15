6d8aedb72a51641d6f07d3ff0c0bb604a69fcf50
Amirhosein Halimi <amirhoseinhalimy138444@gmail.com>
2023-04-15 11:01:07 +0430

    added first code for  event listner2

--- listner.js
+++ listner.js
@@ -2,6 +2,7 @@ let paragraphElement = document.querySelector('p');
 
 function changeElement() {
   paragraphElement.textContent = 'Clicked!';
+  console.log('Paragraph clicked!');
 }
 
 paragraphElement.addEventListener('click', changeElement);
