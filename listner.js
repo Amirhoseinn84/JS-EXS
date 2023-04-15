73efd1df465d05be8ad3a37f6d4d358a61a54d38
Amirhosein Halimi <amirhoseinhalimy138444@gmail.com>
2023-04-15 11:26:28 +0430

    added first code for  event listner 6

--- listner.js
+++ listner.js
@@ -11,7 +11,8 @@ let inputElement = document.querySelector('input');
 
 function retrieveUserInput(event) {
   // let enteredText = inputElement.value;
-  let enteredText = event.target.value;
+  // let enteredText = event.target.value;
+  let enteredText = event.data;
   console.log(enteredText);
   // console.log(event);
 }
