36e55dfa3b27af57fe9b2a4576f035116a53d769
Amirhosein Halimi <amirhoseinhalimy138444@gmail.com>
2023-04-16 19:00:17 +0430

    added new project 5

--- loops.js
+++ loops.js
@@ -16,3 +16,19 @@ function calculateSum() {
 }
 
 calculateSumButtonElement.addEventListener('click', calculateSum);
+
+/////
+
+const highlightLinksButtonElement = document.querySelector(
+  '#highlight-links button'
+);
+
+const anchorElements = document.querySelectorAll('#highlight-links a');
+
+function hgihlightLinks() {
+  for (const anchorElement of anchorElements) {
+    anchorElement.classList.add('highlight');
+  }
+}
+
+highlightLinksButtonElement.addEventListener('click', hgihlightLinks);
