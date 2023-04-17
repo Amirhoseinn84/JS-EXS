4969a17860fe2baa14730308ca9e33c2985bef63
Amirhosein Halimi <amirhoseinhalimy138444@gmail.com>
2023-04-17 13:30:01 +0430

    added new string temp 5

--- full/assets/scripts/app.js
+++ full/assets/scripts/app.js
@@ -9,3 +9,11 @@ let userData = '(' + calculateNum * 2 + ')';
 let textInput = 'error' + ' ' + 'occured!';
 
 outputResult(textInput, userData);
+
+function userName(name) {
+  let nameUser = 'Hi!' + ' ' + name;
+  return nameUser;
+}
+
+let inpuUser = userName('Amirhosein');
+console.log(inpuUser);
