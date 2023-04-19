9974387cd80bf166109553dcfcddd7a0851a37bb
Amirhosein Halimi <amirhoseinhalimy138444@gmail.com>
2023-04-19 11:57:16 +0430

    added more 3

--- full/assets/advancedproject.js
+++ full/assets/advancedproject.js
@@ -4,4 +4,12 @@ function startgame() {
   console.log('Game is starting...');
 }
 
+const person = {
+  greet: function greet() {
+    console.log('Hello there!');
+  },
+};
+
+person.greet();
+
 startGameBtn.addEventListener('click', startgame);
