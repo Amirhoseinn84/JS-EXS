ddc6f5c9bf04aaf71f34b528b30ef45ceb6bf5b5
Amirhosein Halimi <amirhoseinhalimy138444@gmail.com>
2023-04-19 23:11:18 +0430

    added more 30

--- app.js
+++ app.js
@@ -33,6 +33,8 @@ let firstParagraph = document.querySelector('p');
 console.dir(firstParagraph);
 
 firstParagraph.append(newAnchorElement);
+firstParagraph.appendChild(newAnchorElement);
+firstParagraph.prepend(newAnchorElement);
 
 // let removeHeader = document.querySelector('h1');
 
