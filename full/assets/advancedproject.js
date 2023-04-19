4672733ae79cfb13b4ca5a22f2d96c0ea474cb81
Amirhosein Halimi <amirhoseinhalimy138444@gmail.com>
2023-04-19 11:52:06 +0430

    added more 2

---
+++ full/assets/advancedproject.js
@@ -0,0 +1,7 @@
+const startGameBtn = document.getElementById('start-game-btn');
+
+function startgame() {
+  console.log('Game is starting...');
+}
+
+startGameBtn.addEventListener('click', startgame);
