1d6db8f15aa9af7353265ca11e05682e15ddd5d9
Amirhosein Halimi <amirhoseinhalimy138444@gmail.com>
2023-04-20 14:22:42 +0430

    added more 66

--- full/projectarr.js
+++ full/projectarr.js
@@ -196,11 +196,11 @@ console.log(firstName, lastName, otherInformation);
 
 //////////////////////////
 
-const ids = new Set([1, 2, 3]);
+const ids = new Set(['Hi', 'from', 'set!']);
 ids.add(2);
 
 for (const entry of ids.entries()) {
-  console.log(entry);
+  console.log(entry[1]);
 }
 
 // console.log(ids.has(1));
