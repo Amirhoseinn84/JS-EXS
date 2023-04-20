6c54114def3aa479be4054a4d2d18875515d2193
Amirhosein Halimi <amirhoseinhalimy138444@gmail.com>
2023-04-20 12:01:46 +0430

    added more 49

--- full/projectarr.js
+++ full/projectarr.js
@@ -81,3 +81,19 @@ const amirhoseinIndex = personalData2.findIndex((person, idx, persons) => {
 });
 
 console.log(amirhoseinIndex);
+
+const prices = [1.99, 3.99, 5.99, 7.99];
+const tax = 0.19;
+const taxAdjustedPrices = [];
+
+// for (const price of prices) {
+// taxAdjustedPrices.push(price * (1 + tax));
+// }
+
+prices.forEach((price, idx, prices) => {
+  const priceObj = { index: idx, taxAdjPrice: price * (1 + tax) };
+  taxAdjustedPrices.push(priceObj);
+});
+
+// console.log(taxAdjustedPrices);
+console.log(taxAdjustedPrices);
