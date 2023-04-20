c62039e51ce657b3bc9815ea92bff5a5fd9dd973
Amirhosein Halimi <amirhoseinhalimy138444@gmail.com>
2023-04-20 09:59:43 +0430

    added more 37

--- full/projectarr.js
+++ full/projectarr.js
@@ -6,3 +6,9 @@ console.log(moreNumbers);
 
 const moreNumbersInput = new Array(6);
 console.log(moreNumbersInput);
+
+const moreNumbersInput2 = Array(2, 3);
+console.log(moreNumbersInput2);
+
+const yetMoreNumbers = Array.of(3, 4);
+console.log(yetMoreNumbers);
