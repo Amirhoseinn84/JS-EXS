cd13d278128c6ec750f36fdca46ed5d31bf52bd5
Amirhosein Halimi <amirhoseinhalimy138444@gmail.com>
2023-04-21 13:27:08 +0430

    added more 11

--- full/projectobj/assets/scripts/objects.js
+++ full/projectobj/assets/scripts/objects.js
@@ -1,10 +1,9 @@
-"use strict";
 const addMovieBtn = document.getElementById('add-movie-btn');
 const searchBtn = document.getElementById('search-btn');
 
 const movies = [];
 
-const renderMovies = (filter = '') => {
+const renderMovies = () => {
   const movieList = document.getElementById('movie-list');
 
   if (movies.length === 0) {
@@ -15,21 +14,12 @@ const renderMovies = (filter = '') => {
   }
   movieList.innerHTML = '';
 
-  const filteredMovies = !filter
-    ? movies
-    : movies.filter(movie => movie.info.title.includes(filter));
-
-  filteredMovies.forEach(movie => {
+  movies.forEach((movie) => {
     const movieEl = document.createElement('li');
-    const { info, ...otherProps } = movie;
-    console.log(otherProps);
-    // const { title: movieTitle } = info;
-    let { getFormattedTitle } = movie;
-    getFormattedTitle = getFormattedTitle.bind(movie);
-    let text = getFormattedTitle() + ' - ';
-    for (const key in info) {
+    let text = movie.info.title + ' - ';
+    for (const key in movie.info) {
       if (key !== 'title') {
-        text = text + `${key}: ${info[key]}`;
+        text = text + `${key}: ${movie.info[key]}`;
       }
     }
     movieEl.textContent = text;
@@ -55,21 +45,11 @@ const addMovieHandler = () => {
       title,
       [extraName]: extraValue
     },
-    id: Math.random().toString(),
-    getFormattedTitle() {
-      console.log(this);
-      return this.info.title.toUpperCase();
-    }
+    id: Math.random()
   };
 
   movies.push(newMovie);
   renderMovies();
 };
 
-const searchMovieHandler = () => {
-  const filterTerm = document.getElementById('filter-title').value;
-  renderMovies(filterTerm);
-};
-
 addMovieBtn.addEventListener('click', addMovieHandler);
-searchBtn.addEventListener('click', searchMovieHandler);
