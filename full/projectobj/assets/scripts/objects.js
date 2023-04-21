1e1fa1839141bca8b3ba1bdf7561518c278c7361
Amirhosein Halimi <amirhoseinhalimy138444@gmail.com>
2023-04-21 11:11:17 +0430

    added more

---
+++ full/projectobj/assets/scripts/objects.js
@@ -0,0 +1,10 @@
+const person = {
+  name: 'Amirhosein',
+  age: 18,
+  hobbies: ['coding', 'reading', 'trading'],
+  greet: function () {
+    console.log('Hi there!');
+  },
+};
+
+person.greet();
