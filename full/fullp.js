5bd1e06c4db7ed1a70f9c2d4f551707a889c7a11
Amirhosein Halimi <amirhoseinhalimy138444@gmail.com>
2023-04-22 18:14:30 +0430

    added more 30

--- full/fullp.js
+++ full/fullp.js
@@ -1,39 +1,43 @@
-class AgedPerson {
-  printAge() {
-    console.log(this.age);
-  }
-}
+// class AgedPerson {
+//   printAge() {
+//     console.log(this.age);
+//   }
+// }
 
-class Person extends AgedPerson {
-  name = 'Amirhosein';
+// class Person extends AgedPerson {
+//   name = 'Amirhosein';
 
-  constructor() {
-    super();
-    this.age = 18;
-  }
+//   constructor() {
+//     super();
+//     this.age = 18;
+//   }
 
-  greet() {
-    console.log(
-      'Hi, I am ' + this.name + ' and I am ' + this.age + ' years old.'
-    );
-  }
-}
-
-// function Person() {
-//   this.age = 18;
-//   this.name = 'Amirhosein';
-//   this.greet = function () {
+//   greet() {
 //     console.log(
 //       'Hi, I am ' + this.name + ' and I am ' + this.age + ' years old.'
 //     );
-//   };
+//   }
 // }
+
+function Person() {
+  this.age = 18;
+  this.name = 'Amirhosein';
+  this.greet = function () {
+    console.log(
+      'Hi, I am ' + this.name + ' and I am ' + this.age + ' years old.'
+    );
+  };
+}
 
 // Person.prototype = {
 //   printAge() {
 //     console.log(this.age);
 //   },
 // };
+
+Person.prototype.printAge = function () {
+  console.log(this.age);
+};
 
 console.dir(Person);
 
@@ -42,3 +46,6 @@ person.greet();
 person.printAge();
 console.log(person.__proto__);
 // console.log(person.__proto__ === Person.prototype);
+
+const person2 = new person.__proto__.constructor();
+console.log(person2);
