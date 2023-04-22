3eb9b1fd72b129f92855bc780ad30a8e46801abc
Amirhosein Halimi <amirhoseinhalimy138444@gmail.com>
2023-04-22 22:35:13 +0430

    added more 52

--- listner.js
+++ listner.js
@@ -21,9 +21,15 @@ inputElement.addEventListener('input', retrieveUserInput);
 
 const btn = document.querySelector('button');
 
-btn.onclick = function () {
-  console.log('Hi! there');
+// btn.onclick = function () {
+//   console.log('Hi! there');
+// };
+
+const buttonClickeHandler = () => {
+  alert('Button was clicked!');
 };
+
+btn.onclick = buttonClickeHandler;
 
 // btn.addEventListener('click', () => {
 //   console.log('HIii');
