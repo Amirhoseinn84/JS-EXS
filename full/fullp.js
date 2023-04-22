d1d9fec03fead5137a5eb05e9a569d97de1d805f
Amirhosein Halimi <amirhoseinhalimy138444@gmail.com>
2023-04-22 17:24:29 +0430

    added more 27

--- full/fullp.js
+++ full/fullp.js
@@ -1,15 +1,25 @@
-class Person {
-  name = 'Amirhosein';
+// class Person {
+//   name = 'Amirhosein';
 
-  constructor() {
-    this.age = 18;
-  }
+//   constructor() {
+//     this.age = 18;
+//   }
 
-  greet() {
+//   greet() {
+//     console.log(
+//       'Hi, I am ' + this.name + ' and I am ' + this.age + ' years old.'
+//     );
+//   }
+// }
+
+function Person() {
+  this.age = 18;
+  this.name = 'Amirhosein';
+  this.greet = function () {
     console.log(
       'Hi, I am ' + this.name + ' and I am ' + this.age + ' years old.'
     );
-  }
+  };
 }
 
 const peron = new Person();
