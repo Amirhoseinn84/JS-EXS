b06bd67cfd151545fab0e91721741a9a465756f2
Amirhosein Halimi <amirhoseinhalimy138444@gmail.com>
2023-04-23 12:31:41 +0430

    added more 66

--- listner.js
+++ listner.js
@@ -113,3 +113,18 @@ listItems3.addEventListener('click', (event) => {
   // from.submit();
   button.click();
 });
+
+///////////////////
+
+function createTaxCalculator(tax) {
+  function calculateTax(amount) {
+    return amount * tax;
+  }
+  return calculateTax;
+}
+
+const calculateVatAmount = createTaxCalculator(0.19);
+const calculateIncomeTaxAmount = createTaxCalculator(0.25);
+
+console.log(calculateVatAmount(100));
+console.log(calculateIncomeTaxAmount(200));
